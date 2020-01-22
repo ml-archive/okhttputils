960628178ec1a13b7492a550708e305f96abe98e
maru <maru@nodesagency.com>
2020-01-22 14:47:57 +0100

    Update docs

--- app/src/main/java/dk/nodes/okhttputils/session/base/AuthCallback.kt
+++ app/src/main/java/dk/nodes/okhttputils/session/base/AuthCallback.kt
@@ -7,7 +7,7 @@ interface AuthCallback {
      *
      * @param refreshToken the current refreshToken or null if it was not set in [AuthRepository]. Remember to call [AuthRepository.setRefreshToken] as soon as you have a signed in user. Also keep in mind that your [refreshToken] can always expire, in which case you probably have to sign-out your user.
      *
-     * @return [AuthResult] with newly generated AccessToken and/or RefreshToken
+     * @return [AuthResult] with newly generated AccessToken
      */
     fun provideAuthInfo(refreshToken: String?): AuthResult<AuthInfo>
 }
