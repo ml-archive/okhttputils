cbc658edffa50da2848e8e3f5d1cfe5b27ca841e
maru <maru@nodesagency.com>
2020-01-22 14:57:37 +0100

    Update docs and make lint happy

--- app/src/main/java/dk/nodes/okhttputils/session/base/AuthCallback.kt
+++ app/src/main/java/dk/nodes/okhttputils/session/base/AuthCallback.kt
@@ -5,9 +5,9 @@ interface AuthCallback {
     /**
      * Called if new [AuthInfo] is required.
      *
-     * @param refreshToken the current refreshToken or null if it was not set in [AuthRepository]. Remember to call [AuthRepository.setRefreshToken] as soon as you have a signed in user. Also keep in mind that your [refreshToken] can always expire, in which case you probably have to sign-out your user.
+     * @param refreshToken the current Refresh Token or null if it was not set in [AuthRepository]. Remember to call [AuthRepository.setRefreshToken] as soon as you have a signed in user. Also keep in mind that your Refresh Token can always expire, in which case you probably have to sign-out your user.
      *
-     * @return [AuthResult] with newly generated AccessToken
+     * @return [AuthResult] with newly generated Access Token
      */
     fun provideAuthInfo(refreshToken: String?): AuthResult<AuthInfo>
 }
--- app/src/main/java/dk/nodes/okhttputils/session/base/AuthResult.kt
+++ app/src/main/java/dk/nodes/okhttputils/session/base/AuthResult.kt
@@ -1,8 +1,8 @@
 package dk.nodes.okhttputils.session.base
 
-sealed class AuthResult<out T : AuthInfo> {
+sealed class AuthResult<out T> {
 
-    data class Success<out T: AuthInfo>(val data: T) : AuthResult<T>()
+    data class Success<out T>(val data: T) : AuthResult<T>()
     data class Error(val exception: Exception) : AuthResult<Nothing>()
 
     override fun toString(): String {
